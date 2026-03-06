1a87bc219c9c24ddce0b00d810d5a197801a71ee
Bob <tpitorin@myges.fr>
2026-03-06 16:41:27 +0100

    feat: controller + routes

---
+++ api/app/Http/Controllers/Pet/PetController.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Pet;
+
+use App\Enums\ApiStatus;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Pet\StorePetRequest;
+use App\Http\Requests\Pet\UpdatePetRequest;
+use App\Http\Resources\PetResource;
+use App\Models\Pet;
+use App\Services\Pet\PetService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
+
+class PetController extends Controller
+{
+    public function __construct(
+        private PetService $petService
+    ) {}
+
+    public function index(): JsonResponse
+    {
+        $pets = $this->petService->getUserPets(auth()->user());
+
+        return PetResource::collection($pets)
+            ->additional([
+                'status' => ApiStatus::SUCCESS,
+                'timestamp' => human_date(Carbon::now()),
+            ])
+            ->response();
+    }
+
+    public function show(Pet $pet): JsonResponse
+    {
+        $this->authorize('view', $pet);
+
+        $pet = $this->petService->findById($pet->id);
+
+        return (new PetResource($pet))
+            ->additional([
+                'status' => ApiStatus::SUCCESS,
+                'timestamp' => human_date(Carbon::now()),
+            ])
+            ->response();
+    }
+
+    public function store(StorePetRequest $request): JsonResponse
+    {
+        $pet = $this->petService->create($request->user(), $request->validated());
+
+        return (new PetResource($pet))
+            ->additional([
+                'status' => ApiStatus::SUCCESS,
+                'timestamp' => human_date(Carbon::now()),
+            ])
+            ->response()
+            ->setStatusCode(201);
+    }
+
+    public function update(UpdatePetRequest $request, Pet $pet): JsonResponse
+    {
+        $this->authorize('update', $pet);
+
+        $pet = $this->petService->update($pet, $request->validated());
+
+        return (new PetResource($pet))
+            ->additional([
+                'status' => ApiStatus::SUCCESS,
+                'timestamp' => human_date(Carbon::now()),
+            ])
+            ->response();
+    }
+
+    public function destroy(Pet $pet): JsonResponse
+    {
+        $this->authorize('delete', $pet);
+
+        $this->petService->delete($pet);
+
+        return response()->json(null, 204);
+    }
+}
